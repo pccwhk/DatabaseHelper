84d4742edf588162bf985164f870ea26da326b46
pccwhk <test@test.com>
2015-02-18 18:45:00 +0800

    Add test frameowrk

---
+++ src/test/scala/org/ccw/test/database/TestHelper.scala
@@ -0,0 +1,14 @@
+package org.ccw.test.database
+
+import org.scalatest.junit.JUnitSuite
+import scala.collection.mutable.ListBuffer
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
+
+class TestHelper  extends JUnitSuite {
+
+  @Test def verifyTest1() {
+    assertTrue(1==1)
+  }
+}
